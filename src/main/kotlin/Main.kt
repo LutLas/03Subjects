45cddb682010d7a59ce007d7201118e3cdf84084
Muyoba Lutlas <lutakachihota@gmail.com>
2020-07-28 22:33:00 +0200

    subjects Intro

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
+
 fun main(args: Array<String>) {
+
+    exampleOf("PublishSubject") {
+        val disposables = CompositeDisposable()
+        val publishSubject = PublishSubject.create<Int>()
+        publishSubject.onNext(0)
+        val subscriptionOne = publishSubject
+                .doOnDispose { println("Disposed") }
+                .doOnSubscribe { println("Subscribed: $it") }
+                .subscribe { int ->
+            println(int)
+        }
+        publishSubject.onNext(1)
+        publishSubject.onNext(2)
+        disposables.add(subscriptionOne)
+        disposables.dispose()
+    }
 
 }
