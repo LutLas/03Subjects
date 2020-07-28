8eeae6a2b670a75da37fb07581fc655802e3b9af
Muyoba Lutlas <lutakachihota@gmail.com>
2020-07-29 00:30:03 +0200

    Working with publish subjects

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,12 +10,24 @@ fun main(args: Array<String>) {
         val subscriptionOne = publishSubject
                 .doOnDispose { println("Disposed") }
                 .doOnSubscribe { println("Subscribed: $it") }
-                .subscribe { int ->
-            println(int)
+                .subscribe { numbness ->
+            println(numbness)
         }
         publishSubject.onNext(1)
         publishSubject.onNext(2)
+
+        val subscriptionTwo = publishSubject
+                .doOnDispose { println("Disposed Two") }
+                .doOnSubscribe { println("Subscribed: $it") }
+                .subscribe { int ->
+                    printWithLabel("2)", int)
+                }
+        publishSubject.onNext(3)
+
         disposables.add(subscriptionOne)
+        disposables.dispose()
+        publishSubject.onNext(4)
+        disposables.add(subscriptionTwo)
         disposables.dispose()
     }
 
