9adb7cb8d41a9137d1a4612ba72c1579d4fb8815
Muyoba Lutlas <lutakachihota@gmail.com>
2020-07-29 01:09:15 +0200

    Working with behavior subjects

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,18 @@
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 
 fun main(args: Array<String>) {
 
-    exampleOf("PublishSubject") {
+    /*exampleOf("PublishSubject") {
         val disposables = CompositeDisposable()
         val publishSubject = PublishSubject.create<Int>()
         publishSubject.onNext(0)
         val subscriptionOne = publishSubject
-                .doOnDispose { println("Disposed") }
-                .doOnSubscribe { println("Subscribed: $it") }
+                .doOnDispose { println("Disposed One") }
+                .doOnComplete { println("Completed One") }
+                .doOnSubscribe { println("Subscribed One: $it") }
                 .subscribe { numbness ->
             println(numbness)
         }
@@ -18,7 +21,8 @@ fun main(args: Array<String>) {
 
         val subscriptionTwo = publishSubject
                 .doOnDispose { println("Disposed Two") }
-                .doOnSubscribe { println("Subscribed: $it") }
+                .doOnComplete { println("Completed Two") }
+                .doOnSubscribe { println("Subscribed Two: $it") }
                 .subscribe { int ->
                     printWithLabel("2)", int)
                 }
@@ -27,8 +31,46 @@ fun main(args: Array<String>) {
         disposables.add(subscriptionOne)
         disposables.dispose()
         publishSubject.onNext(4)
+
+        // 1
+        publishSubject.onComplete()
+
+// 2
+        publishSubject.onNext(5)
+
+// 3
+
+
+// 4
+        val subscriptionThree = publishSubject.subscribeBy(
+                onNext = { printWithLabel("3)", it) },
+                onComplete = { printWithLabel("3)", "Complete") }
+        )
+
+        publishSubject.onNext(6)
+
+
+        disposables.add(subscriptionThree)
         disposables.add(subscriptionTwo)
         disposables.dispose()
-    }
+    }*/
 
+    // 1
+    exampleOf("BehaviorSubject") {
+        // 2
+        val subscriptions = CompositeDisposable()
+        // 3
+        val behaviorSubject = BehaviorSubject.createDefault("Initial value")
+        behaviorSubject.onNext("X")
+        val subscriptionOne = behaviorSubject
+                .doOnDispose { println("DisposedOne") }
+                .doOnComplete { println("CompletedOne") }
+                .doOnSubscribe { println("SubscribedOne: $it") }
+                .subscribeBy(
+                onNext = { printWithLabel("1)", it) },
+                onError = { printWithLabel("1)", it) }
+        )
+        subscriptions.add(subscriptionOne)
+        subscriptions.dispose()
+    }
 }
