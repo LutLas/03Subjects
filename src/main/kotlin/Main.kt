aecee4ebaa21a9e26b9e2f6e0fa493e215a06c58
Muyoba Lutlas <lutakachihota@gmail.com>
2020-07-29 22:53:01 +0200

    RxRelay

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
+import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.AsyncSubject
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.ReplaySubject
 
 fun main(args: Array<String>) {
 
@@ -105,5 +108,73 @@ fun main(args: Array<String>) {
         subscriptions.dispose()
     }*/
 
-    
+    /*exampleOf("ReplaySubject") {
+
+        val subscriptions = CompositeDisposable()
+        // 1
+        val replaySubject = ReplaySubject.createWithSize<String>(2)
+        // 2
+
+        replaySubject.onNext("1")
+
+        replaySubject.onNext("2")
+
+        replaySubject.onNext("3")
+        // 3
+        subscriptions.add(replaySubject.subscribeBy(
+                onNext = { printWithLabel("1)", it) },
+                onError = { printWithLabel("1)", it)}
+        ))
+
+        subscriptions.add(replaySubject.subscribeBy(
+                onNext = { printWithLabel("2)", it) },
+                onError = { printWithLabel("2)", it)}
+        ))
+
+        replaySubject.onNext("4")
+        replaySubject.onError(RuntimeException("Error!"))
+
+        subscriptions.add(replaySubject.subscribeBy(
+                onNext = { printWithLabel("3)", it) },
+                onError = { printWithLabel("3)", it)}
+        ))
+
+        subscriptions.dispose()
+    }*/
+
+    /*exampleOf("AsyncSubject") {
+        val subscriptions = CompositeDisposable()
+        // 1
+        val asyncSubject = AsyncSubject.create<Int>()
+        // 2
+        subscriptions.add(asyncSubject.subscribeBy(
+                onNext = { printWithLabel("1)", it) },
+                onComplete = { printWithLabel("1)", "Complete") }
+        ))
+        // 3
+        asyncSubject.onNext(0)
+        asyncSubject.onNext(1)
+        asyncSubject.onNext(2)
+        // 4
+        asyncSubject.onComplete()
+
+        subscriptions.dispose()
+    }*/
+
+    /*exampleOf("RxRelay") {
+        val subscriptions = CompositeDisposable()
+
+        val publishRelay = PublishRelay.create<Int>()
+
+        subscriptions.add(publishRelay.subscribeBy(
+                onNext = { printWithLabel("1)", it) }
+        ))
+
+        publishRelay.accept(1)
+        publishRelay.accept(2)
+        publishRelay.accept(3)
+    }*/
+
+
+
 }
